ae0ae5f1b92526432525ca944ca8ecd02a397542
valjeanjean <ilian.metina83@gmail.com>
2025-02-21 11:25:41 +0100

    Update shifumi_new.c

--- shifumi_new.c
+++ shifumi_new.c
@@ -150,6 +150,16 @@ int main(){
         }
     }
 
+    if(multi_or_pc == MULTIPLAYER){
+
+        printf("Veuillez saisir le pseudonyme du premier joueur.\n");
+        ask_username_display(player1_username, MAX_LENGTH); 
+        printf("Veuillez saisir le pseudonyme du deuxième joueur.\n");
+        ask_username_display(player2_username, MAX_LENGTH);
+
+
+    }
+
     while(multi_or_pc == MULTIPLAYER){ 
     
         printf("Veuillez saisir le pseudonyme du premier joueur.\n");
